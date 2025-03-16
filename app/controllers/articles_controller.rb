4d5e245193103520b3ea6acbe12e7a3d96cc78de
spheal363 <115055083+spheal363@users.noreply.github.com>
2025-03-16 16:44:36 +0900

    feature: ログインユーザー名で投稿を保存

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,7 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
+    @article.user_id = current_user.id
 
     respond_to do |format|
       if @article.save
